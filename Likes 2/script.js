9b06934b6972f76b3368e2cbe850e70be1fdbf34
SmithV22 <firewaterdragon111@gmail.com>
2021-10-20 08:26:22 -0400

    first commit

---
+++ Likes 2/script.js
@@ -0,0 +1,16 @@
+function likes() {
+    document.querySelector(".likes").innerText++;
+    console.log("I");
+}
+
+function likes2() {
+    document.querySelector(".likes2").innerText++;
+    console.log("DID");
+}
+
+function likes3() {
+    document.querySelector(".likes3").innerText++;
+    console.log("IT!");
+}
+
+
