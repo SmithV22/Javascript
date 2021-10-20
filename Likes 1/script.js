fd81e6ea37bafd71e04e06e3ab2815664d2c3290
SmithV22 <firewaterdragon111@gmail.com>
2021-10-19 22:06:56 -0400

    first commit

---
+++ Likes 1/script.js
@@ -0,0 +1,14 @@
+// var counter = 3
+// var countorElement = document.querySelector("#numlikes");
+// function likes() {
+//     counter++;
+//     console.log(counter);
+//     return (counter);
+//     countorElement.innerText = counter;
+// }
+function likes() {
+    document.querySelector("#numlikes").innerText++;
+
+}
+
+
